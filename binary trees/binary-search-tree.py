52f65b0b2d176668d21b030efcefbae7a65c9342
Cyprian Munene <munenecyp@gmail.com>
2022-11-27 16:29:35 +0300

    implement binary search traversal

--- binary trees/binary-search-tree.py
+++ binary trees/binary-search-tree.py
@@ -15,3 +15,36 @@ class Node:
                 self.right = another
             else:
                 self.right.add(another)
+
+    def inorder(self, node):
+        """
+        left, parent, right
+        :param node:
+        :return:
+        """
+        if node:
+            self.inorder(node.left)  # call the inorder method recursively on the left subtree till leaf node
+            print(node.value)  # once we reach the leaf, print node value
+            self.inorder(node.right)  # call the inorder method recursively on the right subtree till leaf node
+
+    def preorder(self, node):
+        """
+        root,left,right
+        :param node: the node
+        :return:
+        """
+        if node:
+            print(node.value)  # print node value
+            self.preorder(node.left)  # call the inorder method recursively on the left subtree till leaf node
+            self.preorder(node.right)  # call the inorder method recursively on the right subtree till leaf node
+
+    def postorder(self, node):
+        """
+        left,right,node
+        :param node:
+        :return:
+        """
+        if node:
+            self.preorder(node.left)  # call the inorder method recursively on the left subtree till leaf node
+            self.preorder(node.right)  # call the inorder method recursively on the right subtree till leaf node
+            print(node.value)  # print node value
